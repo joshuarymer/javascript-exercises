fde4336ef77be6bfdd0e5980ce2a7631a3286340
Joshua Rymer <joshuarymer1@gmail.com>
2022-09-22 10:29:21 +0100

    complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let outputString = '';
+    for (let x = 0; x < string.length; x++) {
+        outputString += string.charAt((string.length-1)-x);
+    }
+    return outputString;
 };
 
 // Do not edit below this line
