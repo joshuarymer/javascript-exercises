8a83b9bf72c53856b7e327d62b50b475872de0c6
Joshua Rymer <joshuarymer1@gmail.com>
2022-09-22 11:38:00 +0100

    complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args ) {
+    let index;
+    for (const arg of args) {
+        for (const element of array) {
+            if (arg === element) {
+                index = array.indexOf(arg);
+                array.splice(index, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
