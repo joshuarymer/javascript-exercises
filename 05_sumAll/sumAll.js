d37f88ca3a758af388930d9e8a29f95b3246b4b5
Joshua Rymer <joshuarymer1@gmail.com>
2022-09-22 12:15:28 +0100

    complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let outputNum = 0;
+    if (typeof num1 != 'number' || typeof num2 != 'number' || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    else {
+        if (num1 < num2) {
+            for (x = num1; x <= num2; x++) {
+                outputNum += x;
+            }
+            return outputNum;
+        }
+        else {
+            for (x = num2; x <= num1; x++) {
+                outputNum += x;
+            }
+            return outputNum;
+        }
+    }
 };
 
 // Do not edit below this line
