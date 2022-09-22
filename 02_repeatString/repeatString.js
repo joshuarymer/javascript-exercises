8b7fa1184bdd57e8ed7305b090a6f19f0489dbae
Joshua Rymer <joshuarymer1@gmail.com>
2022-09-22 10:14:12 +0100

    complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {    
+    let outputString = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else {
+        for (x = 0; x < num; x++) {
+            outputString += string;
+        }
+        return outputString;
+        }
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
