a915336c43fca8623682391f11c4229e83a19ce6
Joshua Rymer <joshuarymer1@gmail.com>
2022-10-12 12:21:34 +0100

    Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function(array) {
+	const reducer = array.reduce((a, b) => a + b, 0);
+  return reducer;
 
 };
 
-const power = function() {
-	
+const multiply = function(array) {
+	const reducer = array.reduce((a, b) => a * b);
+  return reducer;
 };
 
-const factorial = function() {
-	
+const power = function(a, b) {
+  let num = a
+  for (i = 1; i < b; i++) {
+    num *= a;
+  }
+  return num;
+};
+
+const factorial = function(a) {
+  let num = a;
+  if(a === 0) {return 1;}
+  else {
+    for(i = a-1; i > 0; i--) {
+      num *= i;
+    }
+    return num; 
+  }
 };
 
 // Do not edit below this line
