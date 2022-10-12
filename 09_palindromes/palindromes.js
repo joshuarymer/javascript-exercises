5bb270faa079ba336a2c677cb963964774abb2d1
Joshua Rymer <joshuarymer1@gmail.com>
2022-10-12 13:17:30 +0100

    Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
-};
+const palindromes = function(string) {
+    string = string
+                .replaceAll('!','')
+                .replaceAll(',', '')
+                .replaceAll('.', '')
+                .replaceAll(' ', '');
+            
+    string = string.toLowerCase();
+    const newArray = string.split("");
+        let output = '';
+        for(i = newArray.length-1; i >= 0; i--) {
+            output += newArray[i];
+        } 
+        if(output === string) {return true;}
+        else {return false;}
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
