6a98dce9ebaed150fe6bf960c88deb42582607c5
Joshua Rymer <joshuarymer1@gmail.com>
2022-10-31 10:47:30 +0000

    Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let array = [0, 1];
+    if (input < 0) return "OOPS";
+    else {
+        for (i = input; i >= 0; i--) {
+            array.push(Number(array[array.length-2]) + Number(array[array.length-1]));
+        }
+        return array[input];
+    }
 };
 
 // Do not edit below this line
