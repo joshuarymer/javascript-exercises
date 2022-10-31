f4d682ae120d58d5c8d5e4432887cee537e0b25d
Joshua Rymer <joshuarymer1@gmail.com>
2022-10-31 12:28:12 +0000

    Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    date = new Date();
+    for (i = 0; i < people.length; i++) {
+        if (people[i].hasOwnProperty('yearOfDeath') === false) {
+            people[i].yearOfDeath = date.getFullYear();
+        }
+    }
+    ordered = people.sort((a,b) =>
+        (a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ? 1 : -1);
+    return ordered[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
